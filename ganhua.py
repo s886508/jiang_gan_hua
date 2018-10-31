63ed9f23341d06490163f6b05cb65fd226c8827d
Will Huang <s951553@gmail.com>
2018-10-31 11:23:33 +0800

    Tokenize ganhua dictionary.

--- ganhua.py
+++ ganhua.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import jieba
 
 class GanHua:
 
-    def __init__(self, _id, _spokes, _position, _message):
+    def __init__(self, _id, _spokes, _position, _message, _tokens):
         self.id = _id
         self.spokesman = _spokes
         self.position = _position
         self.message = _message
+        self.tokens = _tokens
 
 class GanHuaDict:
 
     def __init__(self):
         self.__gan_hua_list = []
+        jieba.set_dictionary("dict.txt.big")
 
     def load_from_file(self, path):
         """ Parse data from given file path.
@@ -26,4 +29,16 @@ class GanHuaDict:
         if file is not None:
             json_data = json.load(file)
             for record in json_data:
-                self.__gan_hua_list.append(GanHua(record["id"], record["spokesman"], record["position"], record["message"]))
+                tokens = self.__tokenize(record["message"])
+                self.__gan_hua_list.append(GanHua(record["id"], record["spokesman"], record["position"], record["message"], tokens))
+
+    def __tokenize(self, message):
+        """
+        Use Jieba to tokenize given message.
+        :param message(str): String to tokenize.
+        :return: Return list of tokens.
+        """
+        tokens = []
+        for t in jieba.cut(message, cut_all=False):
+            tokens.append(t)
+        return tokens
