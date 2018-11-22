2b0d73e4fae2d16629875891c9b15a0e2e847dc4
Will Huang <s951553@gmail.com>
2018-11-22 09:55:04 +0800

    Random select gan hua.

--- ganhua_expert.py
+++ ganhua_expert.py
@@ -4,22 +4,26 @@ from .text_compare.strategy import CompareStrategyAbstract, JaccardCompareStrate
 
 class GanHuaExpert:
 
-    def __init__(self, comp_strategy):
+    def __init__(self, comp_strategy, dict):
+        """
+        :param comp_strategy: Strategy to compare sentence similarity.
+        :param dict: GanHuaDict to compare.
+        """
         self.__comp_stategy = comp_strategy
+        self.__dict = dict
 
-    def query_similiar_from_dict(self, text, num, dict):
+    def query_similiar_from_dict(self, text, num):
         """
         Calculate text similarity from given text and dictionary.
         :param text(str): Text to calculate similarity.
         :param num(int): Top most num similar text calculated.
-        :param dict: GanHuaDict to compare.
         :return: List of top most num text.
         """
         ret = []
         if num <= 0:
             return ret
 
-        for g in dict.get_dict():
+        for g in self.__dict.get_dict():
             score = self.cal_similiarity(text, g.message)
             ret.append(g.message)
         return ret[0 : num]
@@ -34,3 +38,17 @@ class GanHuaExpert:
         if self.__comp_stategy is not None:
             return self.__comp_stategy.compare(t1, t2)
         return 0
+
+    def random_pick(self, count):
+        """
+        :param count: Numbers to return.
+        :return: Set of gan hua.
+        """
+        gan_hua_set = set()
+        for index in range(count):
+            pick = self.__dict.random_pick()
+            while pick in gan_hua_set:
+                pick = self.__dict.random_pick()
+            gan_hua_set.add(pick)
+
+        return gan_hua_set
--- test/test_ganhua_expert.py
+++ test/test_ganhua_expert.py
@@ -10,28 +10,29 @@ class TestGanHuaExpert:
 
         # Dictionary has not loaded.
         text = "薪水高低很重要"
-        analyzer = GanHuaExpert(JaccardCompareStrategy())
-        similars = analyzer.query_similiar_from_dict(text, 1, dict)
+        analyzer = GanHuaExpert(JaccardCompareStrategy(), dict)
+        similars = analyzer.query_similiar_from_dict(text, 1)
         assert len(similars) == 0
 
         # Load dictionary and query.
         dict.load_from_file("data/ganhua_dict.json")
-        similars = analyzer.query_similiar_from_dict(text, 1, dict)
+        similars = analyzer.query_similiar_from_dict(text, 1)
         assert len(similars) == 1
 
-        similars = analyzer.query_similiar_from_dict(text, 30, dict)
+        similars = analyzer.query_similiar_from_dict(text, 30)
         assert len(similars) == 30
 
         # Query more than dictionary.
-        similars = analyzer.query_similiar_from_dict(text, 60, dict)
+        similars = analyzer.query_similiar_from_dict(text, 60)
         assert len(similars) != 60
 
     def test_similarity_single_sentence(self):
         text_src = "反對修法假新聞，政府自己不要造謠"
         text_exmine = "假新聞要關三天"
 
+        dict = GanHuaDict()
 
-        analyzer = GanHuaExpert(LevenshteiinCompareStrategy())
+        analyzer = GanHuaExpert(LevenshteiinCompareStrategy(), dict)
         score = analyzer.cal_similiarity(text_src, text_exmine)
         assert score > 0
 
@@ -41,7 +42,7 @@ class TestGanHuaExpert:
         score = analyzer.cal_similiarity("", text_exmine)
         assert score == len(text_exmine)
 
-        analyzer = GanHuaExpert(JaccardCompareStrategy())
+        analyzer = GanHuaExpert(JaccardCompareStrategy(), dict)
         diff_score = analyzer.cal_similiarity(text_src, text_exmine)
         assert diff_score > 0.8
 
@@ -50,3 +51,17 @@ class TestGanHuaExpert:
 
         diff_score = analyzer.cal_similiarity("", text_exmine)
         assert diff_score == 1.0
+
+    def test_random_pick(self):
+        dict = GanHuaDict()
+        dict.load_from_file("data/ganhua_dict.json")
+        analyzer = GanHuaExpert(LevenshteiinCompareStrategy(), dict)
+
+        gan_hua = analyzer.random_pick(1)
+        assert len(gan_hua) == 1
+
+        gan_hua = analyzer.random_pick(5)
+        assert len(gan_hua) == 5
+
+        gan_hua = analyzer.random_pick(0)
+        assert len(gan_hua) == 0
